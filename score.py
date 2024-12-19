08cb4ef46ad8a7eccadf8b68dd088051a6447119
Apoohow <hollmersi9488@gmail.com>
2024-12-19 11:05:08 +0800

    Add min,max

--- score.py
+++ score.py
@@ -15,7 +15,6 @@ for n in b:
     s = s + int(n)  # 將每個數字加總
     if int(n) < 60:
         p = p + 1  # 如果數字小於 60，計數加 1
-avg = s / len(b)  # 計算平均值
 
 # 找出最大值和最小值
 max_val = max(map(int, b))  # 將字串轉為整數後找最大值
@@ -23,6 +22,5 @@ min_val = min(map(int, b))  # 將字串轉為整數後找最小值
 
 # 輸出結果
 print(f"總和：{s}")
-print(f"平均值：{avg}")
 print(f"最大值：{max_val}")
 print(f"最小值：{min_val}")
